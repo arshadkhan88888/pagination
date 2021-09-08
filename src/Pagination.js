a3f05f4d6300ecbbf1919a0009afc7339bf2eec5
sampan-mangosciences <89766708+sampan-mangosciences@users.noreply.github.com>
2021-09-08 09:59:59 +0530

    11

--- src/Pagination.js
+++ src/Pagination.js
@@ -58,6 +58,24 @@ const Pagination = ({ showPerPage, onChangePagination, total }) => {
               Next
             </a>
           </li>
+          <li className="page-item">
+            <a
+              className="page-link"
+              href="!#"
+              onClick={() => handleChange("next")}
+            >
+              Nexttt
+            </a>
+          </li>
+          <li className="page-item">
+            <a
+              className="page-link"
+              href="!#"
+              onClick={() => handleChange("next")}
+            >
+              Nexttt
+            </a>
+          </li>
         </ul>
       </nav>
     </div>
