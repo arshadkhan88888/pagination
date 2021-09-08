655471de188e699a9fa78c555decb137fab412b7
sampan-mangosciences <89766708+sampan-mangosciences@users.noreply.github.com>
2021-09-08 09:56:28 +0530

    sorry

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Pagination from './Pagination';
 
 const App = () => {
-  return <div>new Pagination</div>;
+  const [data, setData] = useState([])
+  const [showPerPage, setShowPerPage] = useState(50)
+  const [pagination, setPagination] = useState({
+    start: 0,
+    end:showPerPage
+  })
+  
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/comments")
+    .then(res=>setData(res.data))
+  })
+  
+  const onChangePagination = (start, end) => {
+    setPagination({start:start,end:end})
+  }
+  return (
+    <div>
+      <table className="table table-bordered table-dark">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.slice(pagination.start, pagination.end).map((li) => (
+            <tr key={li.id}>
+              <td>{li.id}</td>
+              <td>{li.name}</td>
+              <td>{li.email}</td>
+              <td>{li.body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination
+        showPerPage={showPerPage}
+        onChangePagination={onChangePagination}
+        total={data.length}
+      />
+    </div>
+  );
 };
 
 export default App;
